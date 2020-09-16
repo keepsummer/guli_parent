48143b9cee6ebf2fb51cf27484a5379f8d0c2fd7
李志敏 <915377975@qq.COM>
2020-09-16 17:30:46 +0800

    Initial commit

---
+++ service/service_edu/src/main/java/com/lizhimin/eduService/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.lizhimin.eduService.mapper;
+
+import com.lizhimin.eduService.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-09-16
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/lizhimin/eduService/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.lizhimin.eduService.service.impl;
+
+import com.lizhimin.eduService.entity.EduTeacher;
+import com.lizhimin.eduService.mapper.EduTeacherMapper;
+import com.lizhimin.eduService.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-09-16
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
